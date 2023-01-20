54859f2ad3104ede157d2fcde9bdd7154b3cd76f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-20 18:54:47 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
         })
     return (
         <div className="App">
-            <SuperButton name={"Show me This"} callBack={ShowThisHandler}/>
+            <SuperButton name={"Delete"} callBack={ShowThisHandler}/>
             <ul>
                 {mappedTodos}
             </ul>
