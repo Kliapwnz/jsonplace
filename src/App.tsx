f53fc5d0a2e907c46645fd0f0e2dced1a34f4b71
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-20 18:32:31 +0300

    ShowThisHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, {useEffect} from 'react';
 
 import './App.css';
+import {json} from "stream/consumers";
 
 function App() {
 
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => console.log(json))
-  },[])
+  // useEffect(()=>{
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //       .then(response => response.json())
+  //       .then(json => console.log(json))
+  // },[])
 
 const ShowThisHandler=()=>{
-
+  fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => console.log(json))
 }
 
   return (
