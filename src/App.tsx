38110e473e057442504b18a64489d8686b93bada
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-20 18:20:12 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './App.css';
 
 function App() {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+  })
+
+
 
   return (
     <div className="App">
